6c7daf6474ff276559062c91b7576c19c36b8d7c
JeongseokKang <ubermensch1218@gmail.com>
2024-10-16 18:00:36 +0900

    path fixed

--- Package.swift
+++ Package.swift
@@ -18,14 +18,11 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkManager",
-            path: "Sources/NetworkManager" 
+            name: "NetworkManager"
         ),
         .testTarget(
             name: "NetworkManagerTests",
-            dependencies: ["NetworkManager"],
-            path: "Tests/NetworkManagerTests"
-            
+            dependencies: ["NetworkManager"]
         ),
         
     ]
